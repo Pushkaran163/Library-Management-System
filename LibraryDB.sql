22dc5c1eeeb0f51252689c6ee13e3b906ca6cb7b
Pushkaran_Tyagi163 <117625210+Pushkaran163@users.noreply.github.com>
2024-08-06 17:36:41 +0530

    add file

---
+++ LibraryDB.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE LibraryDB;
+
+USE LibraryDB;
+
+CREATE TABLE Users (
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20)
+);
+
+CREATE TABLE Books (
+	book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    isbn VARCHAR(20),
+    available BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE issues (
+	issue_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    book_id INT,
+    issue_date DATE,
+    return_date DATE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
